9006d715d4c19ca7d5ca944905599d5f2cb84ba5
AnActualEmerald <kevinliger13@gmail.com>
2020-03-27 10:48:39 -0400

    Fixed bug where the wrong event handler was being called

--- events.js
+++ events.js
@@ -1,6 +1,6 @@
-const client = require('./bot.js');
+const client = require("./bot.js");
 
-function handleEvent(event, param1, param2){
+function handle2Event(event, param1, param2) {
     try {
         client.events[event].forEach(e => e(param1, param2));
     } catch (e) {
@@ -9,7 +9,7 @@ function handleEvent(event, param1, param2){
     }
 }
 
-function handleEvent(event, param1){
+function handleEvent(event, param1) {
     try {
         client.events[event].forEach(e => e(param1));
     } catch (e) {
@@ -18,19 +18,37 @@ function handleEvent(event, param1){
     }
 }
 
-client.on("channelCreate", (channel) => handleEvent("channelCreate", channel));
-client.on("channelDelete", (channel) => handleEvent("channelDelete", channel));
-client.on("channelPinsUpdate", (channel, time) => handleEvent("channelPinsUpdate", channel, time));
-client.on("clientUserGuildSettingsUpdate", (settings) => handleEvent("clientUserGuildSettingsUpdate", settings));
-client.on("clientUserSettingsUpdate", (settins) => handleEvent("clientUserSettingsUpdate", settins));
-client.on("emojiCreate", (emoji) => handleEvent("emojiCreate", emoji));
-client.on("emojiDelete", (emoji) => handleEvent("emojiDelete", emoji));
-client.on("emojiUpdate", (emoji) => handleEvent("emojiUpdate", emoji));
+client.on("channelCreate", channel => handleEvent("channelCreate", channel));
+client.on("channelDelete", channel => handleEvent("channelDelete", channel));
+client.on("channelPinsUpdate", (channel, time) =>
+    handle2Event("channelPinsUpdate", channel, time)
+);
+client.on("clientUserGuildSettingsUpdate", settings =>
+    handleEvent("clientUserGuildSettingsUpdate", settings)
+);
+client.on("clientUserSettingsUpdate", settins =>
+    handleEvent("clientUserSettingsUpdate", settins)
+);
+client.on("emojiCreate", emoji => handleEvent("emojiCreate", emoji));
+client.on("emojiDelete", emoji => handleEvent("emojiDelete", emoji));
+client.on("emojiUpdate", emoji => handleEvent("emojiUpdate", emoji));
 client.on("messageDelete", message => handleEvent("messageDelete", message));
-client.on("messageUpdate", (oldM, newM) => handleEvent("messageUpdate", oldM, newM));
-client.on("guildBanAdd", (guild, user) => handleEvent("guildBanAdd", guild, user));
-client.on("guildDelete", (guild) => handleEvent("guildDelete", guild));
-client.on("guildMemberAvailable", (member) => handleEvent("guildMemberAvailable", member));
-client.on("guildMemberRemove", (member) => handleEvent("guildMemberRemove", member));
-client.on("guildMemberSpeaking", channel => handleEvent("guildMemberSpeaking",channel));
-client.on("guildBanRemove", (guild, user) => handleEvent("guildBanRemove", guild, user));
+client.on("messageUpdate", (oldM, newM) =>
+    handle2Event("messageUpdate", oldM, newM)
+);
+client.on("guildBanAdd", (guild, user) =>
+    handle2Event("guildBanAdd", guild, user)
+);
+client.on("guildDelete", guild => handleEvent("guildDelete", guild));
+client.on("guildMemberAvailable", member =>
+    handleEvent("guildMemberAvailable", member)
+);
+client.on("guildMemberRemove", member =>
+    handleEvent("guildMemberRemove", member)
+);
+client.on("guildMemberSpeaking", channel =>
+    handleEvent("guildMemberSpeaking", channel)
+);
+client.on("guildBanRemove", (guild, user) =>
+    handle2Event("guildBanRemove", guild, user)
+);
