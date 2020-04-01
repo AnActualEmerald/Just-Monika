3f3f704bd3d3eaf5c03a9cb4671b53c51672d240
Emerald <kevingauthier1697@gmail.com>
2020-04-01 13:14:51 -0400

    Added the rolecolor command

---
+++ commands/rolecolor.js
@@ -0,0 +1,34 @@
+const Discord = require("discord.js");
+
+
+module.exports = {
+    name: 'rolecolor',
+    description: 'See or set the hex color value of a role',
+    args: true,
+    usage: '<role> [color]',
+    perms: ['MANAGE_ROLES'],
+    category: 'Utilities',
+    execute(message, args, bot){
+        if(args.length == 1){
+            r = message.mentions.roles.first();
+            let embed = new Discord.RichEmbed();
+            embed.setColor(r.hexColor);
+            embed.setDescription(`Color: ${r.hexColor}`);
+            message.channel.send(`Role color for ${r}`, {embed: embed});
+        }
+
+        if(args.length == 2){
+            r = message.mentions.roles.first();
+            r.setColor(`#${args[1]}`).then(update => {
+                let embed = new Discord.RichEmbed();
+                embed.setColor(update.hexColor);
+                embed.setDescription(`Color: ${update.hexColor}`);
+                
+                message.channel.send(`Role color for ${update}`, {embed: embed});
+            });
+
+            
+        }
+    }
+
+}
